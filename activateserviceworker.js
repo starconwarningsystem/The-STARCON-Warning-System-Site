0aec49bc351000c4da10ca4dd2a58cdddb897f14
The STARCON Warning System <starconwarningsystem@gmail.com>
2017-12-07 21:21:00 -0500

    Updated Formatting and Theme, STARCON 5

--- activateserviceworker.js
+++ activateserviceworker.js
@@ -1,14 +1,14 @@
-(function() {
+(function () {
 
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/service-worker.js', {Cache: "max-age=0"}).then(function(registration) {
-        // Registration was successful
-        console.log('Service Worker registered');
-      }, function(err) {
-        // registration failed :(
-        console.log('Service Worker registration failed: ', err);
-      });
-    });
-  }
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register('/service-worker.js', { Cache: "max-age=0" }).then(function (registration) {
+                // Registration was successful
+                console.log('Service Worker registered');
+            }, function (err) {
+                // registration failed :(
+                console.log('Service Worker registration failed: ', err);
+            });
+        });
+    }
 })();
